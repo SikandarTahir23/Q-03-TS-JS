f81b6a8e52053e3d94b73726ac928dde96094661
Sikandar Tahir <159277794+SikandarTahir23@users.noreply.github.com>
2024-02-15 16:37:33 +0500

    Q 03  TS/JS Of 45 Assigments
    Q 03  TS/JS Of 45 Assigments

---
+++ Q3.js
@@ -0,0 +1,7 @@
+"use strict";
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in
+//  lowercase, uppercase, and titlecase.
+let firstNamE = "sikandar tahir";
+console.log(firstNamE.toLocaleLowerCase());
+console.log(firstNamE.toLocaleUpperCase());
+console.log(firstNamE.charAt(0).toUpperCase() + firstNamE.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,8 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in
+//  lowercase, uppercase, and titlecase.
+
+let firstNamE: string = "sikandar tahir";
+
+console.log(firstNamE.toLocaleLowerCase());
+console.log(firstNamE.toLocaleUpperCase());
+console.log(firstNamE.charAt(0).toUpperCase() + firstNamE.slice(1));
